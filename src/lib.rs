c19f1014f2406455f8be6a6a23df0a57e8a94c82
Chris Dickinson <christopher.s.dickinson@gmail.com>
2020-11-14 15:23:13 -0800

    add handler conversions

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,11 @@
 
 use std::collections::HashMap;
 use std::sync::Arc;
+use std::pin::Pin;
+use std::borrow::Cow;
+use futures::Future;
+use futures::prelude::*;
+
 
 // - middleware produces a handler
 // - handlers accept context and produce a response
@@ -10,25 +15,24 @@ use std::sync::Arc;
 //   represent it as a "response" enum
 //
 // acceptable responses:
-// - [x] streams of data
+// - [-] streams of data (NB: this is tricky because streams impl From for (), strings that
+//   conflict with our impls)
 // - [x] json
 // - [x] strings
 // - [x] static strs
 // - [x] static slices
 // - [x] vec<u8>
 // - [x] impl std::error::Error
-// - [x] HashMap< String, any of the above >
+// - [ ] HashMap< String, any of the above >
 // - [ ] Vec< of any of the above >
-// - [ ] unit
-// - [ ] tuple (u16, <any of the above>)
-// - [ ] tuple (HashMap<String, String>, <any of the above>)
-// - [ ] tuple (u16, HashMap<String, String>, <any of the above>)
+// - [x] unit
+// - [x] tuple (u16, <any of the above>)
+// - [x] tuple (u16, HashMap<String, String>, <any of the above>)
 // - [ ] Body of any of the above
 // - [ ] Result< of any of the above >
 
-enum Body {
+pub enum Body {
     Empty,
-    ByteStream(Box<dyn futures::stream::Stream<Item = Vec<u8>>>),
     ByteSlice(&'static [u8]),
     ByteVec(Vec<u8>),
     Str(&'static str),
@@ -39,49 +43,181 @@ enum Body {
     List(Vec<Body>),
 }
 
-// XXX: maybe structure this as a tuple?
-struct Response {
-    body: Body,
-    headers: HashMap<String, String>,
-    status: u16
+impl Into<http_types::Body> for Body {
+    fn into(self) -> http_types::Body {
+        match self {
+            Body::Empty => http_types::Body::empty(),
+            Body::Str(xs) => xs.into(),
+            Body::String(xs) => xs.into(),
+            Body::JSON(xs) => xs.into(),
+            _ => unimplemented!("oh no")
+        }
+    }
 }
 
-struct Context<AppState, RequestState> {
+impl Default for Body {
+    fn default() -> Self {
+        Body::Empty
+    }
+}
+
+// XXX: maybe structure this as a tuple?
+#[derive(Default)]
+pub struct Response<'a> {
+    pub body: Body,
+    pub headers: HashMap<Cow<'a, str>, Cow<'a, str>>,
+    pub status: u16
+}
+
+pub struct Context<AppState, RequestState> {
     application: Arc<AppState>,
     context: RequestState,
     headers: HashMap<String, String>,
 }
 
-use std::pin::Pin;
-use futures::Future;
+impl<AppState, RequestState> Context<AppState, RequestState> {
+    pub fn new(application: Arc<AppState>, context: RequestState, headers: HashMap<String, String>) -> Self {
+        Context {
+            application,
+            context,
+            headers
+        }
+    }
+}
 
-struct Handler<AppState, RequestState> {
+pub struct Handler<'a, AppState: Send + Sync, RequestState: Send + Sync> {
     name: String,
     method: String,
     route: String,
-    action: Box<dyn Fn(Context<AppState, RequestState>) -> anyhow::Result<Response>>
+    pub action: Box<dyn Fn(Context<AppState, RequestState>) -> Pin<Box<dyn Future<Output = Response<'a>> + Send + Sync + 'a>> + Send + Sync>
 }
 
-async fn cast<AppState, RequestState, F, E>(context: Context<AppState, RequestState>, func: impl Fn(Context<AppState, RequestState>) -> F) -> anyhow::Result<Response>
-    where F: futures::Future<Output = anyhow::Result<E>>,
-          E: Into<Response> {
-    func(context).await.map(Into::into)
+impl<'a> From<()> for Response<'a> {
+    fn from(value: ()) -> Response<'a> {
+        Response {
+            status: 204,
+            body: Body::Empty,
+            ..Default::default()
+        }
+    }
 }
 
-impl<AppState, RequestState> Handler<AppState, RequestState> {
-    fn new<F, E>(name: String, method: String, route: String, handler: F) -> anyhow::Result<Self>
-        where F: 'static + Fn(Context<AppState, RequestState>) -> anyhow::Result<E>,
-              E: Into<Response> {
+impl<'a> From<&'static str> for Response<'a> {
+    fn from(value: &'static str) -> Response<'a> {
+        Response {
+            status: 200,
+            body: Body::Str(value),
+            headers: maplit::hashmap!(
+                "content-type".into() => "text/plain; charset=utf-8".into()
+            )
+        }
+    }
+}
 
-        let action = Box::new(move |context| {
-            handler(context).map(Into::into)
-        });
+impl<'a> From<String> for Response<'a> {
+    fn from(value: String) -> Response<'a> {
+        Response {
+            status: 200,
+            body: Body::String(value),
+            headers: maplit::hashmap!(
+                "content-type".into() => "text/plain; charset=utf-8".into()
+            )
+        }
+    }
+}
+
+impl<'a> From<serde_json::Value> for Response<'a> {
+    fn from(value: serde_json::Value) -> Response<'a> {
+        Response {
+            status: 200,
+            body: Body::JSON(value),
+            headers: maplit::hashmap!(
+                "content-type".into() => "application/json; charset=utf-8".into()
+            )
+        }
+    }
+}
+
+impl<'a> From<&'static [u8]> for Response<'a> {
+    fn from(value: &'static [u8]) -> Response<'a> {
+        Response {
+            status: 200,
+            body: Body::ByteSlice(value),
+            headers: maplit::hashmap!(
+                "content-type".into() => "application/octet-stream".into()
+            )
+        }
+    }
+}
+
+impl<'a> From<Vec<u8>> for Response<'a> {
+    fn from(value: Vec<u8>) -> Response<'a> {
+        Response {
+            status: 200,
+            body: Body::ByteVec(value),
+            headers: maplit::hashmap!(
+                "content-type".into() => "application/octet-stream".into()
+            )
+        }
+    }
+}
+
+impl<'a, T> From<Option<T>> for Response<'a>
+    where T: Into<Response<'a>> {
+    fn from(value: Option<T>) -> Response<'a> {
+        value.map(|xs| xs.into()).unwrap_or_else(|| ().into())
+    }
+}
+
+impl<'a, T, E> From<Result<T, E>> for Response<'a>
+    where T: Into<Response<'a>>,
+          E: Into<anyhow::Error> {
+    fn from(value: Result<T, E>) -> Response<'a> {
+        match value {
+            Ok(xs) => xs.into(),
+            Err(e) => Response {
+                status: 500,
+                body: Body::String(e.into().to_string()),
+                headers: maplit::hashmap!(
+                    "content-type".into() => "text/plain; charset=utf-8".into()
+                )
+            }
+        }
+    }
+}
+
+impl<'a, T> From<(u16, T)> for Response<'a>
+    where T: Into<Response<'a>> {
+    fn from(value: (u16, T)) -> Response<'a> {
+        let mut response = value.1.into();
+        response.status = value.0;
+        response
+    }
+}
+
+impl<'a, T> From<(u16, HashMap<Cow<'a, str>, Cow<'a, str>>, T)> for Response<'a>
+    where T: Into<Response<'a>> {
+    fn from(value: (u16, HashMap<Cow<'a, str>, Cow<'a, str>>, T)) -> Response<'a> {
+        let mut response = value.2.into();
+        response.status = value.0;
+        response.headers.extend(value.1);
+        response
+    }
+}
+
+impl<'a, AppState: Send + Sync, RequestState: Send + Sync> Handler<'a, AppState, RequestState> {
+    pub fn new<Fut, F, E>(name: String, method: String, route: String, handler: F) -> anyhow::Result<Self>
+        where F: (Fn(Context<AppState, RequestState>) -> Fut) + Send + Sync + 'static,
+              Fut: Future<Output = E> + Send + Sync + 'static,
+              E: Send + Sync + Into<Response<'a>> {
 
         Ok(Handler {
             name,
             method,
             route,
-            action
+            action: Box::new(move |context| {
+                Box::pin(handler(context).map(|xs| xs.into()))
+            })
         })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,36 @@ use async_std::net::{TcpStream, TcpListener};
 use async_std::prelude::*;
 use async_std::task;
 use http_types::{Response, StatusCode};
+use std::collections::HashMap;
+
+use ludwig::{ Context, Handler };
+use serde_json::json;
+
+async fn hello(_context: Context<(), ()>) -> anyhow::Result<(u16, &'static str)> {
+    Ok((201, "hello world"))
+}
+
+async fn world(_context: Context<(), ()>) -> anyhow::Result<serde_json::Value> {
+    Ok(json!({
+        "message": "hello world"
+    }))
+}
+
+async fn how(_context: Context<(), ()>) -> Result<String, std::io::Error> {
+    Ok(async_std::fs::read_to_string("/usr/share/dict/words").await?)
+}
+
+async fn are(_context: Context<(), ()>) -> &'static str {
+    "okay, I guess"
+}
+
+async fn you(_context: Context<(), ()>) {
+    println!("sometimes you just wanna 204 No Content! I'm not gonna judge")
+}
 
 #[async_std::main]
 async fn main() -> http_types::Result<()> {
+
     // Open up a TCP connection and create a URL.
     let listener = TcpListener::bind(("127.0.0.1", 8080)).await?;
     let addr = format!("http://{}", listener.local_addr()?);
@@ -23,13 +50,27 @@ async fn main() -> http_types::Result<()> {
     Ok(())
 }
 
+use std::borrow::Borrow;
 // Take a TCP stream, and convert it into sequential HTTP request / response pairs.
 async fn accept(stream: TcpStream) -> http_types::Result<()> {
     println!("starting new connection from {}", stream.peer_addr()?);
     async_h1::accept(stream.clone(), |_req| async move {
-        let mut res = Response::new(StatusCode::Ok);
-        res.insert_header("Content-Type", "text/plain");
-        res.set_body("Hello");
+        let handler = Handler::<(), ()>::new("hello".to_string(), "GET".to_string(), "/".to_string(), hello)?;
+        let handler = Handler::<(), ()>::new("hello".to_string(), "GET".to_string(), "/".to_string(), world)?;
+        let handler = Handler::<(), ()>::new("hello".to_string(), "GET".to_string(), "/".to_string(), how)?;
+        let handler = Handler::<(), ()>::new("hello".to_string(), "GET".to_string(), "/".to_string(), are)?;
+        let handler = Handler::<(), ()>::new("hello".to_string(), "GET".to_string(), "/".to_string(), you)?;
+
+        let context = Context::new(std::sync::Arc::new(()), (), HashMap::new());
+        let result = (handler.action)(context).await;
+
+        let mut res = Response::new(result.status);
+        for (key, value) in result.headers {
+            let key: &str = key.borrow();
+            let value: &str = value.borrow();
+            res.insert_header(key, value);
+        }
+        res.set_body(result.body);
         Ok(res)
     })
     .await?;
